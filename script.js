2a95c674005a0d018845d25f27290f940390cf99
vaibhavkambl <142518482+vaibhavkambl@users.noreply.github.com>
2023-10-29 18:53:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
+  const row = document.getElementById("sampleTable");
+	var insertButton = document.getElementById("insertRow");
+
+	  var newRow = row.insertRow(0); 
+
+            var cell1 = newRow.insertCell(0);
+            var cell2 = newRow.insertCell(1);
+
+            // Set cell content
+            cell1.innerHTML = "New Cell1";
+            cell2.innerHTML = "New Cell2";
   
 }
